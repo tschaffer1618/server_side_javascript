f6a3ff089a6d23729662541755b102aa1be269fb
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-15 09:03:49 -0700

    Rename server file and fetch footnotes

--- app.js
+++ app.js
@@ -28,3 +28,13 @@ app.get('/api/v1/papers', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.get('/api/v1/footnotes', (request, response) => {
+  database('footnotes').select()
+  .then((footnotes) => {
+    response.status(200).json(footnotes);
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  });
+});
