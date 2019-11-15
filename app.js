8abef811ac3559b01d95153697598e642ad1798d
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-15 09:23:29 -0700

    Post to papers and footnotes

--- app.js
+++ app.js
@@ -38,3 +38,43 @@ app.get('/api/v1/footnotes', (request, response) => {
     response.status(500).json({ error });
   });
 });
+
+app.post('/api/v1/papers', (request, response) => {
+  const paper = request.body;
+
+  for (let requiredParameter of ['title', 'author']) {
+    if (!paper[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('papers').insert(paper, 'id')
+    .then(paper => {
+      response.status(201).json({ id: paper[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/footnotes', (request, response) => {
+  const footnote = request.body;
+
+  for (let requiredParameter of ['note', 'paper_id']) {
+    if (!footnote[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { note: <String>, paper_id: <Number> }. You're missing a "${requiredParameter}" prpoerty.` });
+    }
+  }
+
+  database('footnotes').insert(footnote, 'id')
+    .then(footnote => {
+      response.status(201).json({ id: footnote[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
