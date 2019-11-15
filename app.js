d64a235356397cde1099e832cc819c2608e7b593
Tyler Schaffer <tpschaffer@gmail.com>
2019-11-15 09:34:05 -0700

    Get papers and footnotes with a specific paper id

--- app.js
+++ app.js
@@ -78,3 +78,35 @@ app.post('/api/v1/footnotes', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.get('/api/v1/papers/:id', (request, response) => {
+  database('papers').where('id', request.params.id).select()
+    .then(papers => {
+      if (papers.length) {
+        response.status(200).json(papers);
+      } else {
+        response.status(404).json({
+          error: `Could not find paper with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/papers/:id/footnotes', (request, response) => {
+  database('footnotes').where('paper_id', request.params.id).select()
+    .then(footnotes => {
+      if (footnotes.length) {
+        response.status(200).json(footnotes);
+      } else {
+        response.status(404).json({
+          error: `Could not find paper with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+})
